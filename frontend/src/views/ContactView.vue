ed32f534a76db2733f8d8a0a13ca21f67b6adefc
Julian Hoffmann <wi21b050@technikum.at>
2023-10-12 17:09:24 +0200

    fixed styling of privacy pages

--- frontend/src/views/ContactView.vue
+++ frontend/src/views/ContactView.vue
@@ -1,39 +1,33 @@
 <template>
-    <div class="home">
-    <h1>Contact us! </h1>
-  </div>
-    <div class="container d-flex justify-content-center">
-        <div class="card detail-product-card mx-auto text-center">
-            <div class="row">
-                <div class=" product-card-body">
-                    <p>
-                        If you have any queation regarding QuizApp don't hesitate to contact us!
-                    </p>
-                    <form id="contact-form" class="contact-form" action="#" method="POST">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label for="name">Name</label>
-                                    <input type="text" class="form-control" id="name" name="name" required>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label for="email">E-Mail</label>
-                                    <input type="email" class="form-control" id="email" name="email" required>
-                                </div>
+    <div class="container d-flex justify-content-center align-items-center">
+        <div class="card loginForm login-card">
+            <div class="card-body">
+                <h1 class="card-title text-center">Contact us!</h1>
+                <form id="contact-form" class="contact-form" action="#" method="POST">
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="name">Name</label>
+                                <input type="text" class="form-control" id="name" name="name" required>
                             </div>
                         </div>
-                        <div class="form-group">
-                            <label for="message">Mesasage</label>
-                            <textarea class="form-control" id="message" name="message" rows="10" required></textarea>
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="email">E-Mail</label>
+                                <input type="email" class="form-control" id="email" name="email" required>
+                            </div>
                         </div>
-                        <div class="card-footer">
-                            <button class="btn btn-primary card-button">send</button>
-                        </div>
-                    </form>
+                    </div>
+                    <div class="form-group">
+                        <label for="message">Mesasage</label>
+                        <textarea class="form-control" id="message" name="message" rows="10" required></textarea>
+                    </div>
+                    <div class="button-container justify-content-evenly">
+                        <button type="submit" class="btn btn-primary card-button">Send</button>
+                    </div>
+
+                </form>
             </div>
         </div>
-    </div>
     </div>
 </template>
--- frontend/src/views/ImprintView.vue
+++ frontend/src/views/ImprintView.vue
@@ -1,37 +1,33 @@
 <template>
-    <div class="home">
-    <h1>Imprint </h1>
-
-  </div>
-     <section>
-        <div class="Form" style="padding-bottom: 50px; padding-top: 50px;">
-
-            <div class="contact text-center">
-                <div class="col-lg-12">
+    <div class="container d-flex justify-content-center align-items-center">
+        <div class="card loginForm login-card">
+            <div class="card-body">
+                <h1 class="card-title text-center">Imprint</h1>
+                <div>
                     <h5>Company name: QuizApp GmbH</h5>
                 </div>
-                <div class="col-lg-12">
+                <div>
                     <h5>Headquarters: Hochstädtplatz 6 | 1200 Vienna</h5>
                 </div>
-                <div class="col-lg-12">
+                <div>
                     <h5>Phone: +43 310 310 3</h5>
                 </div>
-                <div class="col-lg-12">
+                <div>
                     <h5>Fax: +43 310 310 3</h5>
                 </div>
-                <div class="col-lg-12">
-                    <h5>E-Mail-Adress: kundendienst@softwaresupply.at</h5>
+                <div>
+                    <h5>E-Mail-Adress: support@quizapp.at</h5>
                 </div>
-                <div class="col-lg-12">
+                <div>
                     <h5>Commercial Register Nr.: FN 911991g</h5>
                 </div>
-                <div class="col-lg-12">
+                <div>
                     <h5>Commercial Register Court: Handelsgericht Wien</h5>
                 </div>
-                <div class="col-lg-12">
+                <div>
                     <h5>UID-Nr: ATU 911991</h5>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
 </template>
