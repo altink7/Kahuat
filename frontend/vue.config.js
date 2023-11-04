a292b0d023bff13ced468a1a7cefad2ebe67e494
Raphael Koller <koller.raphael@pm.me>
2023-11-04 16:29:06 +0100

    38: Modified vue.config.js and .env for reverse proxy environment

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  devServer: {
+    allowedHosts: process.env.VUE_APP_DOMAIN,
+  },
+  transpileDependencies: true,
 })
