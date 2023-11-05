53061e9c829132251a719e183c20145c17a40e72
Raphael Koller <koller.raphael@pm.me>
2023-11-05 12:04:58 +0100

    Updated vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,8 +1,15 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   devServer: {
-    allowedHosts: process.env.VUE_APP_DOMAIN,
+    // Convert VUE_APP_DOMAIN to an array for allowedHosts
+    allowedHosts: process.env.VUE_APP_DOMAIN ? process.env.VUE_APP_DOMAIN.split(',') : [],
+
+    // Set the WebSocket URL dynamically based on the VUE_APP_DOMAIN
+    client: {
+      webSocketURL: process.env.VUE_APP_DOMAIN ? `ws://${process.env.VUE_APP_DOMAIN}/ws` : 'ws://localhost:8080/ws',
+    },
   },
+
   transpileDependencies: true,
-})
+});
