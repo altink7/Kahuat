e485277948f732e517fa2f1255a477d610f6bd91
Raphael Koller <koller.raphael@pm.me>
2023-11-23 18:46:17 +0100

    Added logout implementation in this branch: #1700587894

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -16,14 +16,30 @@
       <!-- Navbar Links -->
       <div class="collapse navbar-collapse justify-content-evenly text-center" :class="{ 'show': isNavbarOpen }">
         <router-link to="/" class="nav-link">Home</router-link>
-        <router-link to="/login" class="nav-link">Login</router-link>
+        <div v-if="user">
+          <router-link to="/home" @click="logout">{{ user.name }} (logout)</router-link>
+        </div>
+        <div v-else>
+          <router-link to="/login" class="nav-link">Login</router-link>
+        </div>
         <router-link to="/about" class="nav-link">About</router-link>
       </div>
     </div>
   </nav>
 </template>
 
+<script setup>
+import { ref, watchEffect } from 'vue';
+const user = ref(JSON.parse(localStorage.getItem('user')));
+
+watchEffect(() => {
+  console.log('User changed:', user);
+});
+</script>
+
 <script>
+import { googleLogout } from "vue3-google-login"
+
 export default {
     data() {
         return {
@@ -36,6 +52,12 @@ export default {
             const navbarToggler = this.$el.querySelector('.navbar-toggler');
             navbarToggler.classList.toggle('active');
         },
+      logout() {
+        localStorage.removeItem('user');
+        googleLogout();
+        this.$router.push('/login');
+        window.location.reload();
+      }
     },
 };
 </script>
